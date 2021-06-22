11224011ca45cb150fe6e14e293ef75846fea412
Henrique Franco <henrique.frco@gmail.com>
2021-06-22 17:41:14 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,15 +11,12 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Anna", "anna@gmail.com", new Date(), 4300.00, obj);
-		
+	
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(obj);
-		System.out.println(seller);
+		Seller seller = sellerDao.findById(3);
 		
+		System.out.println(seller);
 	}
 
 }
